1aec99b0f203e3d181538366075c693ba423dfcd
TheShmill <21013808+TheShmill@users.noreply.github.com>
2024-12-03 01:48:08 -0500

    Day 3 🥳

---
+++ src/day3.clj
@@ -0,0 +1,21 @@
+(ns day3
+  (:require [clojure.string :as str]))
+
+(def input (slurp "input/day3.txt"))
+
+(defn parse-mul [[_ a b]]
+  (* (parse-long a)
+     (parse-long b)))
+
+(defn part1 [input]
+  (->> input
+       (re-seq #"mul\((\d\d?\d?),(\d\d?\d?)\)")
+       (map parse-mul)
+       (reduce +)))
+
+(defn part2 [input]
+  (->> (str/replace input "\n" "")
+       (re-seq #"(?:do\(\)|^)(.|\n)*?(?:don't\(\)|$)")
+       (map first)
+       (map part1)
+       (reduce +)))

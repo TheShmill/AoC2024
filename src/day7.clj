28d97684dfdb70a38c170a04d98fe0973f5b6853
TheShmill <21013808+TheShmill@users.noreply.github.com>
2024-12-08 15:30:39 -0500

    minor optimizations for day 7

--- src/day7.clj
+++ src/day7.clj
@@ -1,27 +1,32 @@
 (ns day7
-  (:require [clojure.string :as str]))
+  (:require [clojure.string :as str]
+            [clojure.math :as m]))
 
 (def input (slurp "input/day7.txt"))
 
 (defn | [num1 num2]
-  (parse-long (str num1 num2)))
+  (+ (* (int (pow 10 (inc (int (m/log10 num2)))))
+        num1)
+     num2))
 
 (defn parse-line [line]
   (let [[target nums] (str/split line #": ")]
     {:target (parse-long target),
      :nums (rseq (mapv parse-long (str/split nums #" ")))}))
 
-(defn get-results [[val & rest] ops]
+(defn get-results [[val & rest] ops max]
   (if (empty? rest)
     [val]
-    (let [rest (get-results rest ops)]
+    (let [rest (get-results rest ops max)]
       (for [v rest
-            op ops]
-        (op v val)))))
+            op ops
+            :let [result (op v val)]
+            :when (<= result max)]
+        result))))
 
 (defn handle-line [line ops]
   (let [{:keys [target nums]} (parse-line line)]
-    (->> (get-results nums ops)
+    (->> (get-results nums ops target)
          (filter #(= target %))
          first)))
 
